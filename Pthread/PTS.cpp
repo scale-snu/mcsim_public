db8f4c5716c9eb8622628f51c89089e2884d5464
Jung Ho Ahn <geniajh@gmail.com>
2020-07-20 08:33:53 +0900

    cppline Pthread PTS

--- Pthread/PTS.cpp
+++ Pthread/PTS.cpp
@@ -27,81 +27,78 @@
  *
  * Authors: Jung Ho Ahn
  */
-#include "PTS.h"
-#include <iostream>
+
+#include <assert.h>
+#include <stdint.h>
+#include <unistd.h>
 #include <fstream>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <unistd.h>
-#include <stdint.h>
-#include <assert.h>
+#include <utility>
+#include "PTS.h"
 
-using namespace PinPthread;
+namespace PinPthread {
 
-PthreadTimingSimulator::PthreadTimingSimulator(const string & mdfile)
-{
-  cout << "this constructor is not supported any more" << endl;
-  exit(1);
-}
-
-PthreadTimingSimulator::PthreadTimingSimulator(uint32_t _pid, uint32_t _total_num, char * _tmp_shared)
-  :num_piled_instr(0), pid(_pid), total_num(_total_num), tmp_shared(_tmp_shared)
-{
+PthreadTimingSimulator::PthreadTimingSimulator(uint32_t _pid, uint32_t _total_num, char * _tmp_shared):
+  num_piled_instr(0), pid(_pid), total_num(_total_num), tmp_shared(_tmp_shared) {
   // Shared memory
   if ((mmapfd = open(tmp_shared, O_RDWR, 0666)) < 0) {
     perror("open");
     exit(1);
   }
 
-  if((maped = (char*)mmap(0, sizeof(PTSMessage)+2, PROT_WRITE | PROT_READ, MAP_SHARED, mmapfd, 0)) == MAP_FAILED){
+  maped = reinterpret_cast<char *>(mmap(0, sizeof(PTSMessage)+2, PROT_WRITE | PROT_READ, MAP_SHARED, mmapfd, 0));
+  if (maped == MAP_FAILED) {
     perror("mmap");
     exit(1);
   }
 
   ptsmessage = new (maped) PTSMessage;
-  mmap_flag = (bool*)(maped+sizeof(PTSMessage));
+  mmap_flag = reinterpret_cast<bool*>(maped + sizeof(PTSMessage));
 
-  if(pid!=0)
-    sleep(1*pid);
+  // TODO(gajh): Not sure about the meaning of the below code snippet.
+  // if (pid!=0)
+  //   sleep(1*pid);
 
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
   num_hthreads = get_num_hthreads();
   num_available_slot = new uint32_t[num_hthreads];
-  for (uint32_t i = 0; i < num_hthreads; i++)
-  {
+  for (uint32_t i = 0; i < num_hthreads; i++) {
     num_available_slot[i] = 1;
   }
 }
 
 
-PthreadTimingSimulator::~PthreadTimingSimulator()
-{
+PthreadTimingSimulator::~PthreadTimingSimulator() {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type        = pts_destructor;
 
   // Shared memory
   mmap_flag[0] = false;
-  munmap(maped, sizeof(PTSMessage)+2); 
+  munmap(maped, sizeof(PTSMessage)+2);
 }
 
 
-pair<uint32_t, uint64_t> PthreadTimingSimulator::resume_simulation(bool must_switch, bool killed)
-{
+std::pair<uint32_t, uint64_t> PthreadTimingSimulator::resume_simulation(bool must_switch, bool killed) {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type     = pts_resume_simulation;
   ptsmessage->bool_val = must_switch;
   ptsmessage->killed   = killed;
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
-  return pair<uint32_t, uint64_t>(ptsmessage->uint32_t_val, ptsmessage->uint64_t_val);
+  return std::pair<uint32_t, uint64_t>(ptsmessage->uint32_t_val, ptsmessage->uint64_t_val);
+}
+
+
+void PthreadTimingSimulator::sync_with_mcsim() {
+  // shared memory
+  mmap_flag[0] = false;
+  while (mmap_flag[1]) { }
+  mmap_flag[1] = true;
 }
 
 
@@ -113,7 +110,7 @@ bool PthreadTimingSimulator::add_instruction(
     uint64_t raddr,
     uint64_t raddr2,
     UINT32   rlen,
-    uint64_t ip, 
+    uint64_t ip,
     uint32_t category,
     bool     isbranch,
     bool     isbranchtaken,
@@ -122,10 +119,8 @@ bool PthreadTimingSimulator::add_instruction(
     bool     isbarrier,
     uint32_t rr0, uint32_t rr1, uint32_t rr2, uint32_t rr3,
     uint32_t rw0, uint32_t rw1, uint32_t rw2, uint32_t rw3,
-    bool     can_be_piled
-    )
-{
-  //can_be_piled = false;
+    bool     can_be_piled) {
+  // can_be_piled = false;
   assert(num_piled_instr < instr_batch_size);
   ptsmessage->type         = pts_add_instruction;
   PTSInstr   * ptsinstr    = &(ptsmessage->val.instr[num_piled_instr]);
@@ -151,8 +146,7 @@ bool PthreadTimingSimulator::add_instruction(
   ptsinstr->rw1        = rw1;
   ptsinstr->rw2        = rw2;
   ptsinstr->rw3        = rw3;
-  if (num_piled_instr > 0 && (ptsmessage->val.instr[num_piled_instr-1]).hthreadid_ != hthreadid_)
-  {
+  if (num_piled_instr > 0 && (ptsmessage->val.instr[num_piled_instr-1]).hthreadid_ != hthreadid_) {
     cout << curr_time_ << "  " << (ptsmessage->val.instr[num_piled_instr-1]).hthreadid_ << "  " << hthreadid_ << endl;
     exit(1);
   }
@@ -160,12 +154,8 @@ bool PthreadTimingSimulator::add_instruction(
   num_piled_instr++;
   ptsmessage->uint32_t_val = num_piled_instr;
 
-  if (can_be_piled == false || num_piled_instr >= instr_batch_size || num_piled_instr >= num_available_slot[hthreadid_]/* || isbarrier == true*/)
-  {
-    // Shared memory
-    mmap_flag[0] = false;
-    while(mmap_flag[1]){}
-    mmap_flag[1] = true;
+  if (can_be_piled == false || num_piled_instr >= instr_batch_size || num_piled_instr >= num_available_slot[hthreadid_]/* || isbarrier == true*/) {
+    sync_with_mcsim();
 
     num_piled_instr    = 0;
     // return value -- how many more available slots to put instructions,
@@ -173,9 +163,7 @@ bool PthreadTimingSimulator::add_instruction(
     num_available_slot[hthreadid_] = ptsmessage->uint32_t_val;
 
     return (num_available_slot[hthreadid_] <= 1 ? true : false);
-  }
-  else
-  {
+  } else {
     return false;
   }
 }
@@ -184,107 +172,81 @@ bool PthreadTimingSimulator::add_instruction(
 void PthreadTimingSimulator::set_stack_n_size(
     int32_t pth_id,
     ADDRINT stack,
-    ADDRINT stacksize)
-{
+    ADDRINT stacksize) {
   if (num_piled_instr) send_instr_batch();
-  ptsmessage->type         = pts_set_stack_n_size;
-  ptsmessage->uint32_t_val = pth_id;
-  ptsmessage->stack_val    = stack;
-  ptsmessage->stacksize_val= stacksize;
+  ptsmessage->type          = pts_set_stack_n_size;
+  ptsmessage->uint32_t_val  = pth_id;
+  ptsmessage->stack_val     = stack;
+  ptsmessage->stacksize_val = stacksize;
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 }
 
 
-void PthreadTimingSimulator::set_active(int32_t pth_id, bool is_active)
-{
+void PthreadTimingSimulator::set_active(int32_t pth_id, bool is_active) {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type         = pts_set_active;
   ptsmessage->uint32_t_val = pth_id;
   ptsmessage->bool_val     = is_active;
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 }
 
 
-uint32_t PthreadTimingSimulator::get_num_hthreads()
-{
+uint32_t PthreadTimingSimulator::get_num_hthreads() {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type        = pts_get_num_hthreads;
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
   return ptsmessage->uint32_t_val;
 }
 
 
-
-uint64_t PthreadTimingSimulator::get_param_uint64(const string & str, uint64_t def)
-{
+uint64_t PthreadTimingSimulator::get_param_uint64(const std::string & str, uint64_t def) {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type         = pts_get_param_uint64;
   ptsmessage->uint64_t_val = def;
   strcpy(ptsmessage->val.str, str.c_str());
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
   return ptsmessage->uint64_t_val;
 }
 
 
-bool PthreadTimingSimulator::get_param_bool(const string & str, bool def_value)
-{
+bool PthreadTimingSimulator::get_param_bool(const std::string & str, bool def_value) {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type     = pts_get_param_bool;
   ptsmessage->bool_val = def_value;
   strcpy(ptsmessage->val.str, str.c_str());
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
   return ptsmessage->bool_val;
 }
 
 
-uint64_t PthreadTimingSimulator::get_curr_time()
-{
+uint64_t PthreadTimingSimulator::get_curr_time() {
   if (num_piled_instr) send_instr_batch();
   ptsmessage->type         = pts_get_curr_time;
 
-  // Shared memory
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
   return ptsmessage->uint64_t_val;
 }
 
 
-void PthreadTimingSimulator::send_instr_batch()
-{
-  assert(ptsmessage->type  == pts_add_instruction);
+void PthreadTimingSimulator::send_instr_batch() {
+  assert(ptsmessage->type == pts_add_instruction);
 
-  mmap_flag[0] = false;
-  while(mmap_flag[1]){}
-  mmap_flag[1] = true;
+  sync_with_mcsim();
 
   // return value -- how many more available slots to put instructions,
   // 0 means that that we have to resume simulation
-  PTSInstr   * ptsinstr    = &(ptsmessage->val.instr[num_piled_instr-1]);
+  PTSInstr * ptsinstr = &(ptsmessage->val.instr[num_piled_instr-1]);
   num_available_slot[ptsinstr->hthreadid_] = ptsmessage->uint32_t_val;
-  num_piled_instr    = 0;
+  num_piled_instr     = 0;
 }
+
+}  // namespace PinPthread
