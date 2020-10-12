36c70cf6a037ec887abe47307fed6a6acd2f9a7f
djoh0967 <djoh0967@scale.snu.ac.kr>
2020-10-12 22:48:52 +0900

    add tracegen debug mode

--- TraceGen/tracegen.cpp
+++ TraceGen/tracegen.cpp
@@ -7,27 +7,36 @@
 #include <set>
 #include <stdlib.h>
 #include <stdint.h>
+#include <iomanip>
 #include "snappy.h"
 
 #include "pin.H"
 
 using namespace std;
 
+#ifdef DEBUG
+ofstream InstTraceFile;
+KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
+        "o", "instructions.out", "specify trace file name");
+KNOB<BOOL> KnobValues(KNOB_MODE_WRITEONCE, "pintool",
+        "values", "1", "Output memory values reads and written");
+#endif
+
 uint64_t num_instrs;
 uint64_t slice_size;
 string   prefix_name;
 set<uint64_t> slice_index;
 set<uint64_t>::iterator slice_index_iter;
+
 ofstream curr_file;
 uint64_t curr_index;
 bool     tracing;
 
-const uint32_t instr_group_size = 100000;
+static const uint32_t instr_group_size = 100000;
 
 // TODO: define PTSInstrTrace somewhere else!
 // Currently, it is also defined in PTS.h, which is bad!
-struct PTSInstrTrace
-{
+struct PTSInstrTrace {
   uint64_t waddr;
   uint32_t wlen;
   uint64_t raddr;
@@ -48,12 +57,13 @@ struct PTSInstrTrace
 };
 
 PTSInstrTrace instrs[instr_group_size];
-const size_t maxCompressedLength = snappy::MaxCompressedLength(sizeof(PTSInstrTrace)*instr_group_size);  
+const size_t maxCompressedLength = snappy::MaxCompressedLength(sizeof(PTSInstrTrace) * instr_group_size);  
 char * compressed;
 size_t * compressed_length;
+size_t * slice_count;
 
 
-VOID Init(uint32_t argc, char ** argv)
+static VOID Init(uint32_t argc, char ** argv)
 {
   tracing    = false;
   num_instrs = 0;
@@ -61,47 +71,37 @@ VOID Init(uint32_t argc, char ** argv)
   slice_index.clear();
   compressed = new char[maxCompressedLength];
   compressed_length = new size_t;
-  for (uint32_t i = 0; i < argc; i++)
-  {
-    if (argv[i] == string("-prefix"))
-    {
+  slice_count  = new size_t;
+  *slice_count = 0;
+
+  for (uint32_t i = 0; i < argc; i++) {
+    if (argv[i] == string("-prefix")) {
       i++;
       prefix_name = string(argv[i]);
-    }
-    else if (argv[i] == string("-slice_size"))
-    {
+    } else if (argv[i] == string("-slice_size")) {
       i++;
       slice_size  = atol(argv[i]);
 
-      while (true)
-      {
+      while (true) {
         i++;
-        if (argv[i] == string("--"))
-        {
+        if (argv[i] == string("--")) {
           i = argc;
           break;
         }
         slice_index.insert(atol(argv[i]));
       }
-    }
-    else if (argv[i] == string("-h"))
-    {
+    } else if (argv[i] == string("-h")) {
       cout << " usage: -h -prefix prefix_name -slice_size size index0 index1 ..." << endl;
       exit(1);
-    }
-    else if (argv[i] == string("--"))
-    {
+    } else if (argv[i] == string("--")) {
       break;
     }
   }
 
-  if (slice_index.empty() == true)
-  {
+  if (slice_index.empty() == true) {
     cout << "please indicate at least one slice index" << endl;
     exit(1);
-  }
-  else
-  {
+  } else {
     slice_index_iter = slice_index.begin();
   }
 }
@@ -109,7 +109,20 @@ VOID Init(uint32_t argc, char ** argv)
 
 LOCALFUN VOID Fini(int code, VOID * v)
 {
+#ifdef DEBUG
+  InstTraceFile << "Total Instructions: " << num_instrs << endl;
+  InstTraceFile.close();
+#endif
 }
+
+#ifdef DEBUG
+LOCALFUN VOID record_inst (PTSInstrTrace instrs, ADDRINT addr, string op)
+{
+  InstTraceFile << instrs.ip << ": " 
+    << setw(2) << op << " " 
+    << setw(2+2*sizeof(ADDRINT)) << hex << addr << dec << endl;
+}
+#endif
 
 VOID ProcessMemIns(
     ADDRINT ip,
@@ -127,19 +140,18 @@ VOID ProcessMemIns(
     UINT32  rw2,
     UINT32  rw3)
 {
-  if (num_instrs % slice_size == 0)
-  {
+  if (num_instrs % slice_size == 0) {
     cout << "[" << setw(6) << curr_index << "] ";
-    if (curr_index == *(slice_index_iter))
-    {
+
+    if (curr_index == *(slice_index_iter)) {
       tracing = true;
       cout << "here";
       slice_index_iter++;
       char curr_index_str[20];
       sprintf(curr_index_str, "%ld", curr_index);
+
       curr_file.open(string(prefix_name+"."+curr_index_str+".snappy").c_str(), ios::binary);
-      if (curr_file.fail())
-      {
+      if (curr_file.fail()) {
         cout << "failed to open " << prefix_name+"."+curr_index_str+".snappy" << endl;
         exit(1);
       }
@@ -147,8 +159,8 @@ VOID ProcessMemIns(
     cout << endl;
     curr_index++;
   }
-  if (tracing == true)
-  {
+
+  if (tracing == true) {
     PTSInstrTrace & curr_instr = instrs[num_instrs%instr_group_size];
     curr_instr.waddr = waddr;
     curr_instr.wlen  = wlen;
@@ -167,23 +179,42 @@ VOID ProcessMemIns(
     curr_instr.rw1 = rw1;
     curr_instr.rw2 = rw2;
     curr_instr.rw3 = rw3;
+
+#ifdef DEBUG
+    if (wlen != 0 && rlen != 0) {
+      record_inst(curr_instr, waddr, "RW");
+    } else if (wlen == 0 && rlen !=0) {
+      if (raddr2 != 0)
+        record_inst(curr_instr, raddr2, "R2");
+      else 
+        record_inst(curr_instr, raddr, "R1");
+    } else if (wlen != 0 && rlen == 0) {
+      record_inst(curr_instr, waddr, "W");
+    } else if (wlen == 0 && rlen == 0 && isbranch !=0) {
+      record_inst(curr_instr, 0, "B");
+    } else {
+      record_inst(curr_instr, 0, "E");
+    }
+#endif
     
+    cout << curr_instr.ip << ", " << curr_instr.isbranch <<  ", " 
+      << curr_instr.waddr << ", " << curr_instr.raddr << endl;
+
     num_instrs++;
-    if ((num_instrs % instr_group_size) == 0)
-    {
-      snappy::RawCompress((char *)instrs, sizeof(PTSInstrTrace)*instr_group_size, compressed, compressed_length);
-      curr_file.write((char *)compressed_length, sizeof(size_t));
+    if ((num_instrs % instr_group_size) == 0) {
+      *slice_count += 1;
+      snappy::RawCompress(reinterpret_cast<char *>(instrs), sizeof(PTSInstrTrace) * instr_group_size, 
+          compressed, compressed_length);
+      curr_file.write(reinterpret_cast<char *>(slice_count), sizeof(size_t));
+      curr_file.write(reinterpret_cast<char *>(compressed_length), sizeof(size_t));
       curr_file.write(compressed, *compressed_length);
     }
   
-    if (num_instrs % slice_size == 0)
-    {
+    if (num_instrs % slice_size == 0) {
       tracing = false;
       curr_file.close();
       if (slice_index_iter == slice_index.end())
-      {
         exit(1);
-      }
     }
   }
   else
@@ -199,8 +230,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
   bool is_mem_rd   = INS_IsMemoryRead(ins);
   bool has_mem_rd2 = INS_HasMemoryRead2(ins);
 
-  if (is_mem_wr && is_mem_rd && has_mem_rd2) 
-  {
+  if (is_mem_wr && is_mem_rd && has_mem_rd2) {
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ProcessMemIns,
         IARG_INST_PTR,
         IARG_MEMORYREAD_EA,
@@ -220,9 +250,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
         IARG_UINT32, INS_RegW(ins, 2),
         IARG_UINT32, INS_RegW(ins, 3),
         IARG_END);
-  }
-  else if (is_mem_wr && is_mem_rd && !has_mem_rd2) 
-  {
+  } else if (is_mem_wr && is_mem_rd && !has_mem_rd2) {
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ProcessMemIns,
         IARG_INST_PTR,
         IARG_MEMORYREAD_EA,
@@ -232,7 +260,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
         IARG_MEMORYWRITE_SIZE,
         IARG_BOOL, INS_IsControlFlow(ins),
         IARG_BRANCH_TAKEN,
-        IARG_UINT32,  INS_Category(ins),
+        IARG_UINT32, INS_Category(ins),
         IARG_UINT32, INS_RegR(ins, 0),
         IARG_UINT32, INS_RegR(ins, 1),
         IARG_UINT32, INS_RegR(ins, 2),
@@ -242,9 +270,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
         IARG_UINT32, INS_RegW(ins, 2),
         IARG_UINT32, INS_RegW(ins, 3),
         IARG_END);
-  }
-  else if (is_mem_wr && !is_mem_rd) 
-  {
+  } else if (is_mem_wr && !is_mem_rd) {
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ProcessMemIns,
         IARG_INST_PTR,
         IARG_ADDRINT, (ADDRINT)0,
@@ -264,9 +290,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
         IARG_UINT32, INS_RegW(ins, 2),
         IARG_UINT32, INS_RegW(ins, 3),
         IARG_END);
-  }
-  else if (!is_mem_wr && is_mem_rd && has_mem_rd2)
-  {
+  } else if (!is_mem_wr && is_mem_rd && has_mem_rd2) {
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ProcessMemIns,
         IARG_INST_PTR,
         IARG_MEMORYREAD_EA,
@@ -286,9 +310,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
         IARG_UINT32, INS_RegW(ins, 2),
         IARG_UINT32, INS_RegW(ins, 3),
         IARG_END);
-  }
-  else if (!is_mem_wr && is_mem_rd && !has_mem_rd2) 
-  {
+  } else if (!is_mem_wr && is_mem_rd && !has_mem_rd2) {
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ProcessMemIns,
         IARG_INST_PTR,
         IARG_MEMORYREAD_EA,
@@ -308,9 +330,7 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
         IARG_UINT32, INS_RegW(ins, 2),
         IARG_UINT32, INS_RegW(ins, 3),
         IARG_END);
-  }
-  else
-  {
+  } else {
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ProcessMemIns,
         IARG_INST_PTR,
         IARG_ADDRINT, (ADDRINT)0,
@@ -336,10 +356,21 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
 
 GLOBALFUN int main(int argc, char *argv[])
 {
+#ifdef DEBUG
+  string trace_header = string("#\n"
+                               "# Instruction Trace Generated By Pin\n"
+                               "#\n");
+#endif
+
   Init(argc, argv);
   PIN_InitSymbols();
   PIN_Init(argc, argv);
 
+#ifdef DEBUG
+  InstTraceFile.open(KnobOutputFile.Value().c_str());
+  InstTraceFile.write(trace_header.c_str(),trace_header.size());
+  InstTraceFile.setf(ios::showbase);
+#endif
 
   INS_AddInstrumentFunction(Instruction, 0);
   PIN_AddFiniFunction(Fini, 0);
@@ -349,4 +380,3 @@ GLOBALFUN int main(int argc, char *argv[])
 
   return 0; // make compiler happy
 }
-
