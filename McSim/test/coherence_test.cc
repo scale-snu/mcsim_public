72e5b70456cc951af41813754ba379ca36ae4654
wanjudoh <ehdhkswn@naver.com>
2021-09-02 11:23:28 +0900

    update coherence_test

--- McSim/test/coherence_test.cc
+++ McSim/test/coherence_test.cc
@@ -16,10 +16,10 @@ std::vector<O3CoreForTest *> CoherenceTest::test_cores;
 std::vector<CacheL1ForTest *> CoherenceTest::test_l1ds;
 std::vector<CacheL2ForTest *> CoherenceTest::test_l2s;
 DirectoryForTest* CoherenceTest::test_dir;
-uint32_t CoherenceTest::l1_set;
-uint64_t CoherenceTest::l1_tag;
-uint32_t CoherenceTest::l2_set;
-uint64_t CoherenceTest::l2_tag;
+UINT32 CoherenceTest::l1_set;
+UINT64 CoherenceTest::l1_tag;
+UINT32 CoherenceTest::l2_set;
+UINT64 CoherenceTest::l2_tag;
 
 /* 1. START of Cache Build && Fixture Build Testing */
 TEST_F(CoherenceTest, CheckBuild) {
@@ -31,12 +31,12 @@ TEST_F(CoherenceTest, CheckBuild) {
   for (auto && l2 : test_l2s)     ASSERT_NE(nullptr, l2);
   ASSERT_NE(nullptr, test_dir);
 
-  ASSERT_EQ(test_l2s[0],  test_dir->cachel2);
-  ASSERT_EQ(test_dir, test_l2s[0]->directory);
   ASSERT_EQ(test_l2s[0]->cachel1d[0], test_l1ds[0]);
   ASSERT_EQ(test_l2s[0]->cachel1d[1], test_l1ds[1]);
   ASSERT_EQ(test_l1ds[0]->cachel2, test_l2s[0]);
   ASSERT_EQ(test_l1ds[1]->cachel2, test_l2s[0]);
+  ASSERT_EQ(test_l2s[0],  test_dir->cachel2);
+  ASSERT_EQ(test_dir, test_l2s[0]->directory);
 
   ASSERT_EQ(test_l2s[1]->cachel1d[0], test_l1ds[2]);
   ASSERT_EQ(test_l2s[1]->cachel1d[1], test_l1ds[3]);
@@ -51,22 +51,24 @@ TEST_F(CoherenceTest, Case1) {
   l2_set = (TEST_ADDR_D >> test_l2s[0]->set_lsb) % test_l2s[0]->num_sets;
   l2_tag = (TEST_ADDR_D >> test_l2s[0]->set_lsb) / test_l2s[0]->num_sets;
 
-  // coherence state를 확인할 address의 set, tag를 저장
-  // vector 초기화, (마지막 coherence state 저장)
+  // coherence state를 확인할 address 저장
   for (auto && l2 : test_l2s) {
-    l2->address.first  = l2_set;
-    l2->address.second = l2_tag;
+    l2->set_address(TEST_ADDR_D);
   }
-  test_dir->address = TEST_ADDR_D >> test_dir->set_lsb;
+  test_dir->set_address(TEST_ADDR_D);
 
   auto l1_tags_set = test_l1ds[0]->get_tags(l1_set);
   auto l2_tags_set = test_l2s[0]->get_tags(l2_set);
 
-  EXPECT_EQ(l1_tags_set[0]->second, cs_invalid);
-  EXPECT_EQ(l2_tags_set[0]->type_l1l2, cs_invalid);
-  EXPECT_EQ(l2_tags_set[0]->type, cs_invalid);
+  if (l1_tags_set[0]->first == l1_tag) {
+    EXPECT_EQ(l1_tags_set[0]->second, cs_invalid);
+  }
+  if (l2_tags_set[0]->tag == l2_tag) {
+    EXPECT_EQ(l2_tags_set[0]->type, cs_invalid);
+    EXPECT_EQ(l2_tags_set[0]->type_l1l2, cs_invalid);
+  }
 
-  auto it = test_dir->search_dir(TEST_ADDR_D >> test_dir->set_lsb);
+  auto it = test_dir->search_dir(TEST_ADDR_D);
   EXPECT_EQ(it, test_dir->get_dir_end());  // not in directory
 
   set_rob_entry((test_cores[0]->get_o3rob())[0], TEST_ADDR_D, 0);
@@ -83,12 +85,13 @@ TEST_F(CoherenceTest, Case1) {
   EXPECT_EQ(l2_tags_set[0]->type_l1l2, cs_exclusive);
   EXPECT_EQ(l2_tags_set[0]->type, cs_exclusive);
 
-  it = test_dir->search_dir(TEST_ADDR_D >> test_dir->set_lsb);
+  it = test_dir->search_dir(TEST_ADDR_D);
   EXPECT_NE(it, test_dir->get_dir_end());
   EXPECT_EQ(it->second.type, cs_exclusive);
 
-  EXPECT_EQ(test_dir->cs_type.front(), cs_tr_to_e);
-  EXPECT_EQ(test_dir->cs_type.back(), cs_exclusive);    // (I) -> tr_to_e -> E
+  // check transient state
+  EXPECT_EQ(test_dir->cs_type[0], cs_tr_to_e);
+  EXPECT_EQ(test_dir->cs_type[1], cs_exclusive);    // (I) -> tr_to_e -> E
 }
 
 TEST_F(CoherenceTest, Case2) {
@@ -115,7 +118,7 @@ TEST_F(CoherenceTest, Case2) {
   EXPECT_EQ(l2_tags_set[0]->type_l1l2, cs_shared);
   EXPECT_EQ(l2_tags_set[0]->type, cs_exclusive);
 
-  auto it = test_dir->search_dir(TEST_ADDR_D >> test_dir->set_lsb);
+  auto it = test_dir->search_dir(TEST_ADDR_D);
   EXPECT_NE(it, test_dir->get_dir_end());
   EXPECT_EQ(it->second.type, cs_exclusive);
 }
@@ -128,17 +131,17 @@ TEST_F(CoherenceTest, Case3) {
   auto l2_0_tags_set = test_l2s[0]->get_tags(l2_set);
   auto l2_1_tags_set = test_l2s[1]->get_tags(l2_set);
 
-  EXPECT_EQ(l1_2_tags_set[0]->second, cs_invalid);
-
-  EXPECT_EQ(l2_1_tags_set[0]->type_l1l2, cs_invalid);
-  EXPECT_EQ(l2_1_tags_set[0]->type, cs_invalid);
+  if (l1_2_tags_set[0]->first == l1_tag) {
+    EXPECT_EQ(l1_2_tags_set[0]->second, cs_invalid);
+  }
+  if (l2_1_tags_set[0]->tag == l2_tag) {
+    EXPECT_EQ(l2_1_tags_set[0]->type, cs_invalid);
+    EXPECT_EQ(l2_1_tags_set[0]->type_l1l2, cs_invalid);
+  }
 
   set_rob_entry((test_cores[2]->get_o3rob())[0], TEST_ADDR_D, 2400);
   test_cores[2]->set_o3rob_head(0);
   test_cores[2]->set_o3rob_size(1);
-
-  EXPECT_EQ(test_dir->cs_type.back(), cs_exclusive);
-  test_dir->cs_type.clear();
 
   test_pts->mcsim->global_q->add_event(2400, test_cores[2]);
   test_pts->mcsim->global_q->process_event();
@@ -158,8 +161,8 @@ TEST_F(CoherenceTest, Case3) {
   EXPECT_EQ(l2_1_tags_set[0]->type_l1l2, cs_exclusive);
   EXPECT_EQ(l2_1_tags_set[0]->type, cs_shared);
 
-  EXPECT_EQ(test_dir->cs_type.front(), cs_tr_to_s);
-  EXPECT_EQ(test_dir->cs_type.back(), cs_shared);    // (E) -> tr_to_s -> S
+  EXPECT_EQ(test_dir->cs_type[0], cs_tr_to_s);
+  EXPECT_EQ(test_dir->cs_type[1], cs_shared);    // (E) -> tr_to_s -> S
 }
 
 TEST_F(CoherenceTest, Case4) {
@@ -176,9 +179,6 @@ TEST_F(CoherenceTest, Case4) {
   set_rob_entry((test_cores[3]->get_o3rob())[0], TEST_ADDR_D, 3600, false); // write event
   test_cores[3]->set_o3rob_head(0);
   test_cores[3]->set_o3rob_size(1);
-
-  EXPECT_EQ(test_dir->cs_type.back(), cs_shared);
-  test_dir->cs_type.clear();
 
   test_pts->mcsim->global_q->add_event(3600, test_cores[3]);
   test_pts->mcsim->global_q->process_event();
@@ -203,8 +203,8 @@ TEST_F(CoherenceTest, Case4) {
   EXPECT_EQ(l2_1_tags_set[0]->type_l1l2, cs_modified);
   EXPECT_EQ(l2_1_tags_set[0]->type, cs_modified);
 
-  EXPECT_EQ(test_dir->cs_type.front(), cs_tr_to_m);
-  EXPECT_EQ(test_dir->cs_type.back(), cs_modified);    // (S) -> tr_to_m -> M
+  EXPECT_EQ(test_dir->cs_type[0], cs_tr_to_m);
+  EXPECT_EQ(test_dir->cs_type[1], cs_modified);    // (S) -> tr_to_m -> M
 }
 
 TEST_F(CoherenceTest, Case5) {
@@ -218,18 +218,6 @@ TEST_F(CoherenceTest, Case5) {
   test_cores[1]->set_o3rob_head(0);
   test_cores[1]->set_o3rob_size(1);
 
-  EXPECT_EQ(test_l2s[0]->cs_type.back(), cs_invalid);
-  EXPECT_EQ(test_l2s[0]->cs_type_l1l2.back(), cs_invalid);
-  EXPECT_EQ(test_l2s[1]->cs_type.back(), cs_modified);
-  EXPECT_EQ(test_l2s[1]->cs_type_l1l2.back(), cs_modified);
-  EXPECT_EQ(test_dir->cs_type.back(), cs_modified);
-
-  test_l2s[0]->cs_type.clear();
-  test_l2s[0]->cs_type_l1l2.clear();
-  test_l2s[1]->cs_type.clear();
-  test_l2s[1]->cs_type_l1l2.clear();
-  test_dir->cs_type.clear();
-
   test_pts->mcsim->global_q->add_event(4800, test_cores[1]);
   test_pts->mcsim->global_q->process_event();
 
@@ -242,16 +230,16 @@ TEST_F(CoherenceTest, Case5) {
   EXPECT_EQ(l1_3_tags_set[0]->second, cs_exclusive);
 
   EXPECT_EQ(test_l2s[0]->cs_type[0], cs_invalid);
-  EXPECT_EQ(test_l2s[0]->cs_type.back(), cs_shared);          // (I) -> I -> S
+  EXPECT_EQ(test_l2s[0]->cs_type[1], cs_shared);          // (I) -> I -> S
   EXPECT_EQ(test_l2s[0]->cs_type_l1l2[0], cs_invalid);
-  EXPECT_EQ(test_l2s[0]->cs_type_l1l2.back(), cs_exclusive);  // (I) -> I -> E
+  EXPECT_EQ(test_l2s[0]->cs_type_l1l2[1], cs_exclusive);  // (I) -> I -> E
 
-  EXPECT_EQ(test_l2s[1]->cs_type.front(), cs_tr_to_s);
-  EXPECT_EQ(test_l2s[1]->cs_type.back(), cs_shared);          // (M) -> tr_to_s -> S
-  EXPECT_EQ(test_l2s[1]->cs_type_l1l2.back(), cs_exclusive);  // (M) -> E
+  EXPECT_EQ(test_l2s[1]->cs_type[0], cs_tr_to_s);
+  EXPECT_EQ(test_l2s[1]->cs_type[1], cs_shared);          // (M) -> tr_to_s -> S
+  EXPECT_EQ(test_l2s[1]->cs_type_l1l2[0], cs_exclusive);  // (M) -> E
 
-  EXPECT_EQ(test_dir->cs_type.front(), cs_m_to_s);
-  EXPECT_EQ(test_dir->cs_type.back(), cs_shared);             // (M) -> m_to_s -> S
+  EXPECT_EQ(test_dir->cs_type[0], cs_m_to_s);
+  EXPECT_EQ(test_dir->cs_type[1], cs_shared);             // (M) -> m_to_s -> S
 }
 
 TEST_F(CoherenceTest, Case6) {
@@ -262,10 +250,9 @@ TEST_F(CoherenceTest, Case6) {
 
   auto temp = 1 << test_l2s[0]->set_lsb;
   temp *= test_l2s[0]->num_sets;
-  uint64_t const test_address2 = TEST_ADDR_D + temp;  // same index, different tag
+  UINT64 const test_address2 = TEST_ADDR_D + temp;  // same index, different tag
   // test_dir에서 test_address2 block의 coherence state는 I -> tr_to_e -> E 로 변함
-  test_dir->address = test_address2 >> test_dir->set_lsb;
-  test_dir->cs_type.clear();
+  test_dir->set_address(test_address2);
 
   set_rob_entry((test_cores[1]->get_o3rob())[0], test_address2, 6000);
   test_cores[1]->set_o3rob_head(0);
@@ -281,8 +268,8 @@ TEST_F(CoherenceTest, Case6) {
   EXPECT_EQ(l2_1_tags_set[0]->type_l1l2, cs_exclusive);
   EXPECT_EQ(l2_1_tags_set[0]->type, cs_shared);
 
-  EXPECT_EQ(test_dir->cs_type.front(), cs_tr_to_e);
-  EXPECT_EQ(test_dir->cs_type.back(), cs_exclusive);  // (I) -> tr_to_e -> E
+  EXPECT_EQ(test_dir->cs_type[0], cs_tr_to_e);
+  EXPECT_EQ(test_dir->cs_type[1], cs_exclusive);  // (I) -> tr_to_e -> E
 
 /*
   auto test_l1_3 = test_mcsim->l1ds[3];
@@ -317,40 +304,63 @@ TEST_F(CoherenceTest, Case6) {
 */
 }
 
-std::map<uint64_t, PinPthread::Directory::DirEntry>::iterator DirectoryForTest::search_dir(uint64_t addr) {
-  return dir.find(addr);
-}
-std::map<uint64_t, PinPthread::Directory::DirEntry>::iterator DirectoryForTest::get_dir_end() {
-  return dir.end();
-}
-
-uint32_t CacheL2ForTest::process_event(uint64_t curr_time) {
+UINT32 CacheL2ForTest::process_event(UINT64 curr_time) {
   auto res = CacheL2::process_event(curr_time);
+
   for (uint i = 0; i < num_ways; ++i) {
     if (tags[address.first][i]->tag == address.second) {
-      if (cs_type.empty() || cs_type.back() != tags[address.first][i]->type) {
+      if (cs_type.empty() ||
+          cs_type.back() != tags[address.first][i]->type) {
         cs_type.push_back(tags[address.first][i]->type);
       }
       if (cs_type_l1l2.empty() ||
           cs_type_l1l2.back() != tags[address.first][i]->type_l1l2) {
         cs_type_l1l2.push_back(tags[address.first][i]->type_l1l2);
       }
+      break;
+    } else if (i == num_ways - 1) {  // not in L2 cache
+      cs_type.push_back(cs_invalid);
+      cs_type_l1l2.push_back(cs_invalid);
     }
   }
   return res;
 }
 
-uint32_t DirectoryForTest::process_event(uint64_t curr_time) {
+void CacheL2ForTest::set_address(UINT64 address_) {
+  UINT32 set = (address_ >> set_lsb) % num_sets;
+  UINT32 tag = (address_ >> set_lsb) / num_sets;
+  address = std::pair<UINT32, UINT32>(set, tag);
+  cs_type.clear();
+  cs_type_l1l2.clear();
+}
+
+UINT32 DirectoryForTest::process_event(UINT64 curr_time) {
   auto res = Directory::process_event(curr_time);
+
   if (dir.find(address) != dir.end()) {
-    if (cs_type.empty() || cs_type.back() != dir.find(address)->second.type) {
-      cs_type.push_back(dir.find(address)->second.type);
+    if (cs_type.empty() ||  // 아직 기록된 cs_type이 없거나
+        cs_type.back() != dir.find(address)->second.type) {  // 마지막 기록된 cs_type에서 coherence state가 바뀐 경우
+      cs_type.push_back(dir.find(address)->second.type);  // 기록하기
     }
+  } else {  // not in directory
+    cs_type.push_back(cs_invalid);
   }
   return res;
 }
 
-void CoherenceTest::set_rob_entry(O3ROB & o3rob_entry, uint64_t _memaddr, uint64_t ready_time, bool isread) {
+void DirectoryForTest::set_address(UINT64 address_) {
+  address = address_ >> set_lsb;
+  cs_type.clear();
+}
+
+std::map<UINT64, PinPthread::Directory::DirEntry>::iterator DirectoryForTest::search_dir(UINT64 addr) {
+  return dir.find(addr >> set_lsb);
+}
+std::map<UINT64, PinPthread::Directory::DirEntry>::iterator DirectoryForTest::get_dir_end() {
+  return dir.end();
+}
+
+void CoherenceTest::set_rob_entry(O3ROB & o3rob_entry, UINT64 _memaddr, UINT64 ready_time, bool isread) {
   o3rob_entry.memaddr = _memaddr;
   o3rob_entry.isread = isread;
   o3rob_entry.ready_time = ready_time;
--- McSim/test/coherence_test.h
+++ McSim/test/coherence_test.h
@@ -19,44 +19,48 @@ namespace CoherenceTest {
 
 class O3CoreForTest : public O3Core {
  public:
-  explicit O3CoreForTest(component_type type_, uint32_t num_, McSim * mcsim_):
+  explicit O3CoreForTest(component_type type_, UINT32 num_, McSim * mcsim_):
     O3Core(type_, num_, mcsim_) { }
   ~O3CoreForTest() { }
   O3ROB * get_o3rob() { return o3rob; };
-  void set_o3rob_head(uint32_t head) { o3rob_head = head; }
-  void set_o3rob_size(uint32_t size) { o3rob_size = size; }
+  void set_o3rob_head(UINT32 head) { o3rob_head = head; }
+  void set_o3rob_size(UINT32 size) { o3rob_size = size; }
 };
 
 class CacheL1ForTest : public CacheL1 {
  public:
-  explicit CacheL1ForTest(component_type type_, uint32_t num_, McSim * mcsim_):
+  explicit CacheL1ForTest(component_type type_, UINT32 num_, McSim * mcsim_):
     CacheL1(type_, num_, mcsim_) { }
   ~CacheL1ForTest() { }
-  l1_tag_pair** get_tags(uint32_t set) { return tags[set]; }
+  l1_tag_pair** get_tags(UINT32 set) { return tags[set]; }
 };
 
 class CacheL2ForTest : public CacheL2 {
  public:
-  explicit CacheL2ForTest(component_type type_, uint32_t num_, McSim * mcsim_):
-    CacheL2(type_, num_, mcsim_), address(0, 0), cs_type(), cs_type_l1l2() { }
+  explicit CacheL2ForTest(component_type type_, UINT32 num_, McSim * mcsim_):
+    CacheL2(type_, num_, mcsim_), cs_type(), cs_type_l1l2(), address(0, 0) { }
   ~CacheL2ForTest() { }
-  uint32_t process_event(uint64_t curr_time) override;
-  std::pair<uint32_t, uint32_t> address;           // <set, tag>
+  UINT32 process_event(UINT64 curr_time) override;
+  void set_address(UINT64);
   std::vector<coherence_state_type> cs_type;
   std::vector<coherence_state_type> cs_type_l1l2;
-  L2Entry** get_tags(uint32_t set) { return tags[set]; }
+  L2Entry** get_tags(UINT32 set) { return tags[set]; }
+ private:
+  std::pair<UINT32, UINT32> address;           // <set, tag>
 };
 
 class DirectoryForTest : public Directory {
  public:
-  explicit DirectoryForTest(component_type type_, uint32_t num_, McSim * mcsim_):
-    Directory(type_, num_, mcsim_), address(0), cs_type() { }
+  explicit DirectoryForTest(component_type type_, UINT32 num_, McSim * mcsim_):
+    Directory(type_, num_, mcsim_), cs_type(), address(0) { }
   ~DirectoryForTest() { }
-  uint32_t process_event(uint64_t curr_time) override;
-  uint64_t address;
+  UINT32 process_event(UINT64 curr_time) override;
+  void set_address(UINT64);
   std::vector<coherence_state_type> cs_type;
-  std::map<uint64_t, PinPthread::Directory::DirEntry>::iterator search_dir(uint64_t);
-  std::map<uint64_t, PinPthread::Directory::DirEntry>::iterator get_dir_end();
+  std::map<UINT64, PinPthread::Directory::DirEntry>::iterator search_dir(UINT64);
+  std::map<UINT64, PinPthread::Directory::DirEntry>::iterator get_dir_end();
+ private:
+  UINT64 address;
 };
 
 class CoherenceTest : public ::testing::Test {
@@ -66,18 +70,18 @@ class CoherenceTest : public ::testing::Test {
     static std::vector<CacheL1ForTest *> test_l1ds;
     static std::vector<CacheL2ForTest *> test_l2s;
     static DirectoryForTest* test_dir;
-    static uint32_t l1_set;
-    static uint64_t l1_tag;
-    static uint32_t l2_set;
-    static uint64_t l2_tag;
+    static UINT32 l1_set;
+    static UINT64 l1_tag;
+    static UINT32 l2_set;
+    static UINT64 l2_tag;
     // 26C8(hex) = 9928(dec) = 10 011011 001000(bin)
-    static const uint64_t TEST_ADDR_D = 0x26C8;
+    static const UINT64 TEST_ADDR_D = 0x26C8;
 
     static void SetUpTestSuite() {
       test_pts = std::make_unique<PinPthread::PthreadTimingSimulator>("../Apps/md/test/test-coherence.toml");
 
       // fake cores & l1s
-      for (uint32_t i = 0; i < test_pts->mcsim->get_num_hthreads(); i++) {
+      for (UINT32 i = 0; i < test_pts->mcsim->get_num_hthreads(); i++) {
         test_cores.push_back(new O3CoreForTest(ct_o3core, i, test_pts->mcsim));
         test_l1ds.push_back(new CacheL1ForTest(ct_cachel1d, i, test_pts->mcsim));
         auto temp_core = test_pts->mcsim->o3cores[i];
@@ -107,10 +111,15 @@ class CoherenceTest : public ::testing::Test {
 
     virtual void TearDown() override {
       clear_geq();
+      for (auto && l2 : test_l2s) {
+        l2->cs_type.clear();
+        l2->cs_type_l1l2.clear();
+      }
+      test_dir->cs_type.clear();
     }
 
     void clear_geq() { test_pts->mcsim->global_q->event_queue.clear(); }
-    void set_rob_entry(O3ROB & o3rob_entry, uint64_t _memaddr, uint64_t ready_time, bool isread = true);
+    void set_rob_entry(O3ROB & o3rob_entry, UINT64 _memaddr, UINT64 ready_time, bool isread = true);
 };
 
 }
